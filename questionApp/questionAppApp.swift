d5ad4187b325787d84751bd08fbd27737bffcb57
mahnazsaiyed <mahnazsaiyed@gmail.com>
2023-07-14 10:29:55 -0700

    Initial Commit

---
+++ questionApp/questionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionAppApp.swift
+//  questionApp
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct questionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
